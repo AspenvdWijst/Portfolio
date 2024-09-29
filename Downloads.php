90451d107a0888bae608f9e8716f83b14ab304fa
Aspen <s1211810@student.windesheim.nl>
2024-09-29 15:08:57 +0200

    More commit

---
+++ Downloads.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'Header.html'; ?>
+<section>
+    <img src="Images_fonts/450793594_793226346213287_225845.jpg" class="bgimg" alt="test"/>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'Header.html'; ?>
+<section>
+    <!--https://www.youtube.com/watch?v=MiTJnYHX3iA&ab_channel=dcode-->
+        <img src="Images_fonts/450793594_793226346213287_225845.jpg" class="bgimg" alt="test"/>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
