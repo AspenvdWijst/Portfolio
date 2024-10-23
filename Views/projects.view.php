cda907430d714bd3a4782feb519077f8025e5b91
Aspen <aspenvanderwijst@gmail.com>
2024-10-23 16:44:41 +0200

    started on the projects page

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -9,6 +9,22 @@
 <section class="projectscontainer">
     <h1 class="pagetitle">My Projects</h1>
     <h1 id="softwaredevelop">Software development</h1>
+        <section class="developmentcontainer">
+            <div class="portfoliop">
+                <input type="checkbox" id="collapse">
+                <label for="collapse">
+                    <h1>1 | Portfolio webpage</h1>
+                    <hr>
+                    <h2>2024</h2>
+                </label>
+                <div class="collapsed-text">
+                    <h6>lorum ipsum</h6>
+                    <p>
+                        fiebwdufewsdagfvubsdauvgusdagbfvuikgsduvcbkjsdabvckjhbwejkcbgujkaisbvkujsadgvjkubvjkasbvkiujabweukjvbukasbvjkdschabvjkhsadbvkjasdbjh
+                    </p>
+                </div>
+            </div>
+        </section>
     <h1 id="mediaproj">Media</h1>
 </section>
 </body>
