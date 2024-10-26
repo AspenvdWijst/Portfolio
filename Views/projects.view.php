e1cb100593795ae40b9528b0cf828df8c3572ba5
Aspen <aspenvanderwijst@gmail.com>
2024-10-27 00:34:43 +0200

    Finished all the text, media queries are next

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -18,29 +18,51 @@
                     <h2>2024</h2>
                 </label>
                 <div class="portfoliocollapsed">
-                    <h6>lorum ipsum</h6>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad atque cumque distinctio dolore ducimus enim id, modi necessitatibus neque nostrum, officiis omnis quasi ratione repellat tempora velit. Atque, odit!
-                    </p>
+                    <pre>
+My most recent project is this website. I have worked and brainstormed about this website so much.
+Working on this has lead me to become better at not only things like: HTML, CSS, javascript, PHP and SQL.
+But it has also caused me to improve my designing skills. I have reworked the look of the website a few times to where it is now.
+Every time I looked at something else, and this has caused me to be able to make quick edits on the website whenever I need to.
+There is always room for improvement, and in the future there will most likely be a bunch of it too.
+                    </pre>
                 </div>
             </div>
         </section>
     <h1 id="mediaproj">Media</h1>
     <section class="mediacontainer">
         <div class="charityp">
-            <input type="checkbox" id="collapse">
-            <label for="collapse">
-                <h1>1 | Portfolio webpage<img src="../layout/Images/arrow.png" alt="arrow" class="charitydrop"></h1>
+            <input type="checkbox" id="charitycollapse">
+            <label for="charitycollapse">
+                <h1>2 | Radio show for charity<img src="../layout/Images/arrow.png" alt="arrow" class="charitydrop"></h1>
                 <hr>
-                <h2>2024</h2>
+                <h2>2022</h2>
             </label>
             <div class="charitycollapsed">
-                <h6>lorum ipsum</h6>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, assumenda atque deserunt dignissimos distinctio dolorum, eius est itaque nisi nulla pariatur placeat, possimus praesentium quasi reiciendis tenetur voluptatibus? Facilis, maiores!
-                </p>
+                <pre>
+I did a 10 hour radio marathon that raised money for charity. I had the role of not just presenter, but also head of production.
+I made sure everyone stayed on course with the preparation for the show, and jumped in where needed. Even during the day I was flying around.
+In the end we raised €1.345 for the charity War Child. We had talked with people directly affected by the war,
+but we also talked to scholars about the war that was happening in Ukraine. We even talked to the CEO of the Dutch branch of War Child.
+In all of this, I made sure that the right people talked to the right people. And that everything went as planned, and if not. I'd find a solution.
+                </pre>
             </div>
         </div>
+        <div class="strifep">
+            <input type="checkbox" id="strifecollapse">
+            <label for="strifecollapse">
+                <h1>3 | Strife Magazine<img src="../layout/Images/arrow.png" alt="arrow" class="strifedrop"></h1>
+                <hr>
+                <h2>2022</h2>
+            </label>
+            <div class="strifecollapsed">
+                <pre>
+I've been working for <a href="https://www.strifemag.com" target="_blank">Strife Magazine</a> sine 2022. What started as a coincidence, quickly lead to a great bond with the owners
+of the online music magazine. Since then I have written concert reviews, shot concert photography and I've prepared and conducted interviews.
+I have also upheld proper communication with multiple management teams of various bands.
+                </pre>
+            </div>
+        </div>
+
     </section>
 </section>
 </body>
