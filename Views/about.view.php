2d31984cec3eb7d9dc1f4c209b77284aa4a6e64c
Aspen <aspenvanderwijst@gmail.com>
2024-10-26 22:26:25 +0200

    finished the about me text

--- Views/about.view.php
+++ Views/about.view.php
@@ -10,13 +10,29 @@
 <section class="aboutcontainer">
     <h1 class="pagetitle">About Me</h1>
     <pre>
-        I was born on the 27th of March in 2004. For a long time, I had the idea that I'd become a radio presenter. I was actively working to improve my presenting skills and my knowledge of all kinds of music.<br>
-        However, that dream faded after I had tried everything I could. I kept hearing "no" from everything that I tried.<br> This made me get a lot of skills related to the production of events.
-        <br><br>I did a study to learn how to produce events, and I even lead some bigger productions, like a 10-hour-long radio show for charity. This caused me to be very capable at working, and leading, teams.<br>
-        After this I was wondering what to do next. I thought long and hard about my past passions. And I remembered that I have always done a lot with software.<br>
-        Ever since I got my first phone, I've been working on modifying devices. I rooted my first phone and I put homebrew on my old Nintendo 3DS.<br>
-        I had previously tried to get into code. But I never knew where, and more importantly, how to start.<br><br>
+        I was born on the 27th of March in 2004. For a long time, I had the idea that I'd become a radio presenter.
+        I was actively working to improve my presenting skill and my knowledge of all kinds of music. However, that dream faded after I had tried
+        everything I could. I kept hearing "no" from everything that I tried.
 
+        This made me get a lot of skills related to the production of events.
+        I did a study to learn how to produce events, and I even lead some bigger productions, like a 10-hour-long radio show for charity.
+        This caused me to be very capable at working, and leading, teams.
+
+        I even started writing for this online music magazine called <a href="https://www.strifemag.com" target="_blank">Strife Magazine</a>. They also taught me a lot about producing interviews
+        and reviews on a higher level. My love for music is never something that I will lose,
+        and I draw a lot of inspiration from the music that I'm listening to at the time.
+
+        After this I was wondering what to do next. I thought long and hard about my past passions,
+        and I remembered that I have always done a lot with software.
+        Ever since I got my first phone, I've been working on modifying devices. I rooted my first phone and I put homebrew on my old Nintendo 3DS.
+        I had previously tried to get into code. But I never knew where, and more importantly, how to start.
+
+        That made me decide to start this study, software development. I immediately started learning a lot about web development.
+        Within a few weeks I had learned a bunch of HTML and CSS. Then we moved on to things like javascript, php, and sql.
+        Which proved themselves more difficult than the first two. However, after a little bit of work. I eventually learned how to work with those too.
+
+        That is where I am at now. Still learning a lot, and continuing to improve. Creating this portfolio also taught me a lot about coding,
+        and what does, or doesn't work.
     </pre>
 </section>
 </body>
