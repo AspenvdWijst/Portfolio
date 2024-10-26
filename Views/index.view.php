51d939c4f4da25d65f4e6d709314839524b4fb1b
Aspen <aspenvanderwijst@gmail.com>
2024-10-26 17:07:33 +0200

    finished the downloads page

--- Views/index.view.php
+++ Views/index.view.php
@@ -10,14 +10,14 @@
     <h1 class="starttext">Hi! I'm Aspen<br>And I like <span id="typewriter"></span></h1>
     <script src="../Views/javascript/typewriter.js"></script>
     <span class="homeimages">
-        <img src="../Images_fonts/Images/html.png" alt="html">
-        <img src="../Images_fonts/Images/css.png" alt="css">
-        <img src="../Images_fonts/Images/js.png" alt="js">
-        <img src="../Images_fonts/Images/php.png" alt="php">
-        <img src="../Images_fonts/Images/sql.png" alt="sql">
-        <img src="../Images_fonts/Images/ps.png" alt="ps">
-        <img src="../Images_fonts/Images/pp.png" alt="pp">
-        <img src="../Images_fonts/Images/ae.png" alt="ae">
+        <img src="../layout/Images/html.png" alt="html">
+        <img src="../layout/Images/css.png" alt="css">
+        <img src="../layout/Images/js.png" alt="js">
+        <img src="../layout/Images/php.png" alt="php">
+        <img src="../layout/Images/sql.png" alt="sql">
+        <img src="../layout/Images/ps.png" alt="ps">
+        <img src="../layout/Images/pp.png" alt="pp">
+        <img src="../layout/Images/ae.png" alt="ae">
     </span>
 </section>
 </body>
